1040313c463f0f5f6a6dd42ab9be0999be5f5360
Uyiosa Enabulele <hiskoneptz@gmail.com>
2016-11-16 09:11:30 +0100

    chore(jshint): Add es6 to jshint config Fixes #134209505

--- config/env/all.js
+++ config/env/all.js
@@ -1,9 +1,9 @@
 var path = require('path'),
-rootPath = path.normalize(__dirname + '/../..');
+  rootPath = path.normalize(__dirname + '/../..');
 var keys = rootPath + '/keys.txt';
 
 module.exports = {
-	root: rootPath,
-	port: process.env.PORT || 3000,
-    db: process.env.MONGOHQ_URL
+  root: rootPath,
+  port: process.env.PORT || 3000,
+  db: process.env.MONGOHQ_URL || '127.0.0.1:27017/test'
 };
