58b2b5a19e7776c1a5772c7663054e269668d269
Uyiosa Enabulele <hiskoneptz@gmail.com>
2016-11-17 10:51:09 +0100

    chore(houndci-errors):fix houndci errors in all.js Fixes #134209505
    removed an unused but declared variable

--- config/env/all.js
+++ config/env/all.js
@@ -1,7 +1,6 @@
 const path = require('path');
 
 const rootPath = path.normalize(path.join(__dirname, '/../..'));
-const keys = path.join(rootPath, '/keys.txt');
 
 module.exports = {
   root: rootPath,
