46eeecdf83c576168012cedbff889929c9eff8d8
Victor Nwaiwu <victornwaiwu@victor.local>
2016-11-23 16:55:58 +0100

    bug(fix-hound-ci): fix style issue with hound ci

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
-/* eslint global-require */
+/* eslint amd:true */
 /**
  * Module dependencies.
  */
 var express = require('express'),
-    fs = require('fs'),
-    passport = require('passport'),
-    logger = require('mean-logger'),
-    io = require('socket.io');
+  fs = require('fs'),
+  passport = require('passport'),
+  logger = require('mean-logger'),
+  io = require('socket.io');
 require('dotenv').config({ silent: true });
 
 /**
