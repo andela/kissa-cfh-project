d7409aef8eb090a02c3d5d8d9df5063bf5f0d4fd
Victor Nwaiwu <victornwaiwu@victor.local>
2016-11-24 17:10:26 +0100

    bug(fix-style-issues): rewrite gulpfile to es6 format
    finishes

--- gulpfile.js
+++ gulpfile.js
@@ -1,85 +1,84 @@
-var gulp = require('gulp');
-var mocha = require('gulp-mocha');
-var nodemon = require('gulp-nodemon');
-var sequence = require('run-sequence');
-var bower = require('gulp-bower');
-var browserSync = require('browser-sync');
-var istanbul = require('gulp-istanbul');
-var jshint = require('gulp-jshint');
-var filter = require('gulp-filter');
-var mainBowerFiles = require ('main-bower-files');
+/* eslint amd:true */
+const gulp = require('gulp');
+const mocha = require('gulp-mocha');
+const nodemon = require('gulp-nodemon');
+const bower = require('gulp-bower');
+const browserSync = require('browser-sync');
+const istanbul = require('gulp-istanbul');
+const jshint = require('gulp-jshint');
+const filter = require('gulp-filter');
 require('dotenv').config();
-var port = process.env.PORT;
+const port = process.env.PORT;
 
-var plugins = require('gulp-load-plugins')();
+const plugins = require('gulp-load-plugins')();
 
-var src = {
-    scss: './public/css/common.scss'
-}
+const src = {
+  scss: './public/css/common.scss'
+};
 
-gulp.task('sass', function(){
-    return gulp.src(src.scss)
+const out = {
+  css: './public/css/'
+};
+
+gulp.task('sass', () => {
+  return gulp.src(src.scss)
     .pipe(plugins.sass())
     .pipe(gulp.dest(out.css));
 });
 
-var out = { 
-    css: './public/css/'
-}    
-
-gulp.task('watch', function(){
-    gulp.watch('app/views/**', browserSync.reload);
-    gulp.watch(['public/js/**', 'app/**/*.js'], browserSync.reload);
-    gulp.watch('public/views/**', browserSync.reload);
-    gulp.watch(src.sass, ['sass']);
-    gulp.watch('public/css/**', browserSync.reload);
+gulp.task('watch', () => {
+  gulp.watch('app/views/**', browserSync.reload);
+  gulp.watch(['public/js/**', 'app/**/*.js'], browserSync.reload);
+  gulp.watch('public/views/**', browserSync.reload);
+  gulp.watch(src.sass, ['sass']);
+  gulp.watch('public/css/**', browserSync.reload);
 });
 
-gulp.task('nodemon', function () {
+gulp.task('nodemon', () => {
   nodemon({
-    script: 'server.js'
-  , ext: 'js'
-  , env: { 'NODE_ENV': 'development' }
-  })
-})
+    script: 'server.js',
+    ext: 'js',
+    env: { NODE_ENV: 'development' }
+  });
+});
 
-gulp.task('pre-test',function(){
+gulp.task('pre-test', () => {
   return gulp.src(['test/**/*.js'])
-  .pipe(istanbul({includeUntested: true}))
+  .pipe(istanbul({ includeUntested: true }))
   .pipe(istanbul.hookRequire());
 });
 
 
-gulp.task('mochaTest',['pre-test'],function(){
+gulp.task('mochaTest', ['pre-test'], () => {
   return gulp.src(['./test/**/*.js'],
     {
       read: false
     })
-  .pipe(mocha({reporter: 'spec'}))
+  .pipe(mocha({ reporter: 'spec' }))
   .pipe(istanbul.writeReports({
     dir: './coverage',
-    reporters: [ 'lcov' ],
+    reporters: ['lcov'],
     reportOpts: { dir: './coverage' },
   }))
-  .once('error', function() {
+  .once('error', () => {
     process.exit(1);
   })
-  .once('end', function() {
+  .once('end', () => {
     process.exit();
-  })
-});
-
-gulp.task('bower', function() {
-  return bower({
-    'cmd': 'install',
-    'directory': './public/lib',
-    'verbosity': 2
   });
 });
 
-gulp.task('serve', ['build', 'watch'], function(){
-    browserSync({
-    proxy: 'localhost:'+port,
+gulp.task('bower', () => {
+  return bower({
+    cmd: 'install',
+    directory: './public/lib',
+    verbosity: 2
+  });
+});
+
+gulp.task('serve', ['build', 'watch'], () => {
+  browserSync({
+    proxy: `localhost: ${port}`,
     port: 5000,
     ui: {
       port: 5001
@@ -92,9 +91,9 @@ gulp.task('default', ['serve']);
 
 gulp.task('build', ['sass', 'nodemon', 'bower']);
 
-gulp.task('lint',function(){
-  var jsFilter = filter(['gruntfile.js', 'public/js/**/*.js',
-   'test/**/*.js', 'app/**/*.js']);
+gulp.task('lint', () => {
+  const jsFilter = filter(['gruntfile.js', 'public/js/**/*.js',
+    'test/**/*.js', 'app/**/*.js']);
   return gulp.src('./**/*.js')
   .pipe(jsFilter)
   .pipe(jshint());
