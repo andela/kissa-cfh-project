c1fadd57c75a7a537344b20a3d99f357c354f6b2
andela-jare <jorg.are@andela.com>
2016-11-29 11:32:37 +0100

    feature(implement-jwt-signup):  implement test for jwt signup Finishes #134209515

---
+++ app/controllers/api/auth.js
@@ -0,0 +1,48 @@
+//auth.js app
+const mongoose = require('mongoose');
+
+const User = mongoose.model('User');
+const jwt = require('jsonwebtoken');
+const config = require('../../../config/env/all');
+//  signup method
+module.exports.signUp = (req, res) => {
+  User.findOne({ email: req.body.email }, (err, registeredUser) => {
+    if (err) return err;
+    // Check if email does not exists
+    if (!registeredUser) {
+      const email = req.body.email;
+      const name = req.body.name;
+      const password = req.body.password;
+      const avatar = req.body.avatar;
+      if (email && name && password) {
+        const user = new User();
+        user.email = email;
+        user.name = name;
+        user.password = password;
+        user.avatar = avatar;
+        //  save the user in the database
+        user.save((err, newUser) => {
+          if (err) return err;
+          const token = jwt.sign(newUser, config.secret, {
+            expiresIn: '24h'
+          });
+          res.json({
+            success: true,
+            message: 'You have successfully signed up!',
+            token
+          });
+        });
+      } else {
+        res.json({
+          success: false,
+          message: 'Authentication failed. No field can be empty!'
+        });
+      }
+    } else {
+      res.json({
+        success: false,
+        message: 'User already available'
+      });
+    }
+  });
+};
---
+++ test/api/auth.js
@@ -0,0 +1,75 @@
+/*  eslint-disable no-unused-vars*/
+const server = require('../../server');
+
+const chai = require('chai');
+const mongoose = require('mongoose');
+
+const User = mongoose.model('User');
+
+const chaiHttp = require('chai-http');
+
+const should = chai.should();
+chai.use(chaiHttp);
+//Test for jwt signup
+describe('Authenticate', () => {
+  before((done) => {
+    User.remove({}, (err) => {
+      if (err) return err;
+      done();
+    });
+  });
+  describe('on signup', () => {
+    it('should have no fields empty', (done) => {
+      const user = {
+        name: 'kissa231',
+        email: 'everything@kissa2314.com',
+        avatar: '',
+        password: ''
+      };
+      chai.request(server)
+      .post('/api/auth/signup')
+      .send(user)
+      .end((err, res) => {
+        res.should.have.status(200);
+        res.body.should.have.property('message');
+        res.body.message.should.equal('Authentication failed. No field can be empty!');
+        done();
+      });
+    });
+    it('should give token on successful signup', (done) => {
+      const user = {
+        name: 'kissa',
+        email: 'everything@kissa.com',
+        avatar: '',
+        password: 'password1'
+      };
+      chai.request(server)
+      .post('/api/auth/signup')
+      .send(user)
+      .end((err, res) => {
+        res.should.have.status(200);
+        res.body.should.have.property('message');
+        res.body.should.have.property('token');
+        res.body.message.should.equal('You have successfully signed up!');
+        done();
+      });
+    });
+    it('should not be an existing user', (done) => {
+      const user = {
+        name: 'kissa',
+        email: 'everything@kissa.com',
+        avatar: '',
+        password: 'password1'
+      };
+      chai.request(server)
+      .post('/api/auth/signup')
+      .send(user)
+      .end((err, res) => {
+        res.should.have.status(200);
+        res.body.should.have.property('message');
+        res.body.message.should.equal('User already available');
+        done();
+      });
+    });
+  });
+});
