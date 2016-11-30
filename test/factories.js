d5248a3eea95e0b0ac953e7c0433917e3347c041
andela-tisrael <israel.tomilayo@andela.com>
2016-11-30 17:34:56 +0100

    written test for the search api

--- test/factories.js
+++ test/factories.js
@@ -1,10 +1,10 @@
-// var mongoose = require('mongoose');
-//   User = mongoose.model('User');
-// var faker = require("faker");
+var mongoose = require('mongoose');
+  User = mongoose.model('User');
+var faker = require("faker");
 
-// exports.user = {
-// 	name : faker.name.findName(),
-// 	email : faker.internet.email(),
-// 	password : faker.internet.password()
-// };
+exports.user = {
+	name : faker.name.findName(),
+	email : faker.internet.email(),
+	password : faker.internet.password()
+};
 
