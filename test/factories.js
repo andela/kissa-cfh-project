9aab998c61e194310cf5ca6b131cc07fd4ea610f
andela-tisrael <israel.tomilayo@andela.com>
2016-11-30 17:36:44 +0100

    feature(search-for-users): writing tests for the search api

--- test/factories.js
+++ test/factories.js
@@ -1,7 +1,6 @@
 var mongoose = require('mongoose');
   User = mongoose.model('User');
 var faker = require("faker");
-
 exports.user = {
 	name : faker.name.findName(),
 	email : faker.internet.email(),
