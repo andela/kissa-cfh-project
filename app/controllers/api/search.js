0178f659d73029a1d2245ab41b865369888cc497
Victor Nwaiwu <victornwaiwu@victor.local>
2016-12-02 11:49:39 +0100

    feature: create search controller
    create search controller for search route api
    
    finish #134209517

---
+++ app/controllers/api/search.js
@@ -0,0 +1,25 @@
+/**
+ * Module dependencies.
+ */
+const mongoose = require('mongoose');
+
+const User = mongoose.model('User');
+
+/**
+ * Gets all users from the database
+ */
+exports.users = (req, res) => {
+  if (req.user && req.user._id) {
+    const query = req.params.email || '';
+    User.find({ email: { $regex: query } }).limit(10)
+      .exec((err, result) => {
+        if (err) {
+          return res.json(err);
+        } else if (!result) {
+          // hope this never happens.
+          return res.send('I did not find any data');
+        }
+        res.json(result);
+      });
+  }
+};
