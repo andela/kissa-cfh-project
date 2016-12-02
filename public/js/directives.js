430f2726f8bbfef4d3ef61b6450646e5410f1276
Victor Nwaiwu <victornwaiwu@victor.local>
2016-12-02 18:09:00 +0100

    feature: create invite, search tasks
    create modal, search function, invite function  and restrict start button to game creator only
    
    #134209517

--- public/js/directives.js
+++ public/js/directives.js
@@ -1,9 +1,9 @@
 angular.module('mean.directives', [])
-  .directive('player', function (){
-    return{
+  .directive('player', function() {
+    return {
       restrict: 'EA',
       templateUrl: '/views/player.html',
-      link: function(scope, elem, attr){
+      link (scope, elem, attr) {
         scope.colors = ['#7CE4E8', '#FFFFa5', '#FC575E', '#F2ADFF', '#398EC4', '#8CFF95'];
       }
     };
@@ -11,69 +11,75 @@ angular.module('mean.directives', [])
     return {
       restrict: 'EA',
       templateUrl: '/views/answers.html',
-      link: function(scope, elem, attr) {
-
-        scope.$watch('game.state', function() {
+      link (scope, elem, attr) {
+        scope.$watch('game.state', function () {
           if (scope.game.state === 'winner has been chosen') {
-            var curQ = scope.game.curQuestion;
-            var curQuestionArr = curQ.text.split('_');
-            var startStyle = "<span style='color: "+scope.colors[scope.game.players[scope.game.winningCardPlayer].color]+"'>";
-            var endStyle = "</span>";
-            var shouldRemoveQuestionPunctuation = false;
-            var removePunctuation = function(cardIndex) {
-              var cardText = scope.game.table[scope.game.winningCard].card[cardIndex].text;
-              if (cardText.indexOf('.',cardText.length-2) === cardText.length-1) {
-                cardText = cardText.slice(0,cardText.length-1);
-              } else if ((cardText.indexOf('!',cardText.length-2) === cardText.length-1 ||
-                cardText.indexOf('?',cardText.length-2) === cardText.length-1) &&
-                cardIndex === curQ.numAnswers-1) {
+            let curQ = scope.game.curQuestion;
+            let curQuestionArr = curQ.text.split('_');
+            let startStyle = `<span style='color: ${scope.colors[scope.game.players[scope.game.winningCardPlayer].color]}'>`;
+            let endStyle = '</span>';
+            let shouldRemoveQuestionPunctuation = false;
+            let removePunctuation = function (cardIndex) {
+              let cardText = scope.game.table[scope.game.winningCard].card[cardIndex].text;
+              if (cardText.indexOf('.', cardText.length - 2) === cardText.length - 1) {
+                cardText = cardText.slice(0, cardText.length - 1);
+              } else if ((cardText.indexOf('!', cardText.length - 2) === cardText.length - 1 ||
+                cardText.indexOf('?', cardText.length - 2) === cardText.length - 1) &&
+                cardIndex === curQ.numAnswers - 1) {
                 shouldRemoveQuestionPunctuation = true;
               }
               return cardText;
             };
             if (curQuestionArr.length > 1) {
-              var cardText = removePunctuation(0);
-              curQuestionArr.splice(1,0,startStyle+cardText+endStyle);
+              let cardText = removePunctuation(0);
+              curQuestionArr.splice(1, 0, startStyle + cardText + endStyle);
               if (curQ.numAnswers === 2) {
                 cardText = removePunctuation(1);
-                curQuestionArr.splice(3,0,startStyle+cardText+endStyle);
+                curQuestionArr.splice(3, 0, startStyle + cardText + endStyle);
               }
-              curQ.text = curQuestionArr.join("");
+              curQ.text = curQuestionArr.join('');
               // Clean up the last punctuation mark in the question if there already is one in the answer
               if (shouldRemoveQuestionPunctuation) {
-                if (curQ.text.indexOf('.',curQ.text.length-2) === curQ.text.length-1) {
-                  curQ.text = curQ.text.slice(0,curQ.text.length-2);
+                if (curQ.text.indexOf('.', curQ.text.length - 2) === curQ.text.length - 1) {
+                  curQ.text = curQ.text.slice(0, curQ.text.length - 2);
                 }
               }
             } else {
-              curQ.text += ' '+startStyle+scope.game.table[scope.game.winningCard].card[0].text+endStyle;
+              curQ.text += ` ${startStyle}${scope.game.table[scope.game.winningCard].card[0].text}${endStyle}`;
             }
           }
         });
       }
     };
-  }).directive('question', function() {
+  }).directive('question', function () {
     return {
       restrict: 'EA',
       templateUrl: '/views/question.html',
-      link: function(scope, elem, attr) {}
+      link (scope, elem, attr) {}
     };
   })
-  .directive('timer', function(){
-    return{
+  .directive('timer', function() {
+    return {
       restrict: 'EA',
       templateUrl: '/views/timer.html',
-      link: function(scope, elem, attr){}
+      link (scope, elem, attr) {}
     };
   }).directive('landing', function() {
     return {
       restrict: 'EA',
-      link: function(scope, elem, attr) {
+      link (scope, elem, attr) {
         scope.showOptions = true;
 
         if (scope.$$childHead.global.authenticated === true) {
           scope.showOptions = false;
         }
       }
+    };
+  })
+  .directive('invites', function () {
+    return {
+      restrict: 'EA',
+      templateUrl: '/views/invite-list.html',
+      link (scope, elem, attr) {}
     };
   });
