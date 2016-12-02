4c19779fa00277ff502c570a12403aaf910b150c
Victor Nwaiwu <victornwaiwu@victor.local>
2016-12-02 11:46:49 +0100

    feature: create invite controller to send mails
    finish #134209517

---
+++ app/controllers/api/invites.js
@@ -0,0 +1,31 @@
+/**
+ * Module dependencies
+ */
+
+const nodemailer = require('nodemailer');
+require('dotenv').config({ silent: true });
+
+
+exports.emailinvite = (req, res) => {
+  const gameLink = req.body.link;
+  const email = req.body.email;
+  const link = `${gameLink}&email=${email}`;
+  const transporter = nodemailer.createTransport('smtps://victor.nwaiwu9%40gmail.com:Ogechukwu1987@smtp.gmail.com');
+
+  const mailOptions = {
+    from: 'victor.nwaiwu9@gmail.com',
+    to: req.body.email,
+    subject: 'Invitation to join Game',
+    html: `<h3> Cards for Humanity </h3><br/>
+    You have been invited by <a>${req.user.name}</a> to join a game in cards for humanity<br/>
+    click on this link <a href="${link}">here</a> to join the game now.<br/>
+    <strong>Cards For Humanity</strong>`
+  };
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      res.json({ status: 'error' });
+    } else {
+      res.json({ status: info.response, user: req.user });
+    }
+  });
+};
