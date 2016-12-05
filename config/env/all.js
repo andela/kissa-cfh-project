2b1ccdc65b9e0e9f925ba1e43f4a8da0ba2af99b
Uyiosa Enabulele <hiskoneptz@gmail.com>
2016-12-05 09:33:52 +0100

    bug(coveralls): delete all files to reinitialize database [Fixes #134678595]

--- config/env/all.js
+++
@@ -1,9 +0,0 @@
-const path = require('path');
-
-const rootPath = path.normalize(path.join(__dirname, '/../..'));
-
-module.exports = {
-  root: rootPath,
-  port: process.env.PORT || 3000,
-  db: process.env.MONGOHQ_URL || 'mongodb://localhost:27017/cfh'
-};
