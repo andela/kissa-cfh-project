8c0bd86bf5c373e7234fca2b096a28b38a8e54d4
andela-nekekwe <ngozi.ekekwe@andela.com>
2016-12-05 19:32:31 +0100

    feature(login-api): implement jwt for login api
    Start #134209513

--- app/controllers/api/auth.js
+++ app/controllers/api/auth.js
@@ -1,15 +1,43 @@
 
 'use strict'
-
 /**
  * Module dependencies.
  */
 const mongoose = require('mongoose');
-
 const jwt = require('jsonwebtoken');
-
-
 const User = mongoose.model('User');
+
+exports.login = (request, response) => {
+  User.findOne({ email: request.body.email},(error, user) => {
+    if (error) {
+      response.status(500).send(error);
+    }
+    if(!user) {
+      response.status(401).json({
+        success: false,
+        message: 'Authentication failed'
+      });
+    } else if (user) {
+      if(!user.authenticate(request.body.password)) {
+        response.status(401).json({
+          success: false,
+          message: 'Authentication failed. Invalid Password'
+        });
+      }
+      else {
+        const token = jwt.sign(user, 'kjzdfhkjhfghzkjvhkashd,hdjgvmbxmvzbvbc', {
+            expiresIn: '24h'
+          });
+          response.status(200).json({
+            success: true,
+            message: 'Authentication successful. User logged in',
+            token: token
+          });
+      }
+    }
+  });
+}
+
 
 
 
