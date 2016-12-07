328168d1898e60166f906be57e834c373408a853
Victor Nwaiwu <victornwaiwu@victor.local>
2016-12-07 17:55:35 +0100

    fix(test): edit user & article files to work with mongoose
    edit the users and articles files to work with mongoose

--- test/user/model.js
+++ test/user/model.js
@@ -25,7 +25,7 @@ describe('<Unit Test>', function() {
 
         describe('Method Save', function() {
             it('should be able to save whithout problems', function(done) {
-                return user.save(function(err) {
+              user.save((err) => {
                     should.not.exist(err);
                     done();
                 });
@@ -33,7 +33,7 @@ describe('<Unit Test>', function() {
 
             it('should be able to show an error when try to save witout name', function(done) {
                 user.name = '';
-                return user.save(function(err) {
+              user.save((err) => {
                     should.exist(err);
                     done();
                 });
