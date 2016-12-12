db086af6c16b0b3c69bf448449bcfb9462b42c8e
andela-nekekwe <ngozi.ekekwe@andela.com>
2016-12-12 21:51:20 +0100

    test(login): write test for login api with jwt
    write test for login api with jwt
    
    [Finishes #134209513]

--- test/api/auth.js
+++ test/api/auth.js
@@ -1,5 +1,6 @@
 /*  eslint-disable no-unused-vars*/
 /*  eslint amd:true */
+
 const server = require('../../server');
 const chai = require('chai');
 const mongoose = require('mongoose');
@@ -10,14 +11,13 @@ const should = chai.should();
 chai.use(chaiHttp);
 
 describe('Authenticate', () => {
-  before((done) => {
-    User.remove({}, (error) => {
-      if (error) return error;
-      done();
-    });
-  });
-
   describe('Signup', () => {
+    before((done) => {
+      User.remove({}, (error) => {
+        if (error) return error;
+        done();
+      });
+    });
     it('should fail authetication when required fields are empty', (done) => {
       const user = {
         name: 'kissa231',
@@ -73,4 +73,90 @@ describe('Authenticate', () => {
       });
     });
   });
+
+  describe('Login', () => {
+    before((done) => {
+      const user = new User({
+        name: 'kissa andela',
+        username: 'jjjk',
+        email: 'kissa@andela',
+        password: '123456',
+      });
+      user.save((error) => {
+        if (error) {
+          throw error;
+        }
+        done();
+      });
+    });
+
+    it('Should return an error on wrong email address', (done) => {
+      const user = {
+        email: 'kissass@andela',
+        password: '123456'
+      };
+      chai.request(server)
+      .post('/api/auth/login')
+      .send(user)
+      .end((error, response) => {
+        response.should.have.status(401);
+        response.body.should.have.property('message');
+        response.body.should.have.property('success');
+        response.body.should.have.property('message').eql('Authentication failed');
+        done();
+      });
+    });
+
+    it('Should return an error if password is invalid', (done) => {
+      const user = {
+        email: 'kissa@andela',
+        password: 'pass'
+      };
+      chai.request(server)
+      .post('/api/auth/login')
+      .send(user)
+      .end((error, response) => {
+        response.should.have.status(401);
+        response.body.should.have.property('message');
+        response.body.should.have.property('success');
+        response.body.should.have.property('message').eql('Authentication failed. Invalid Password');
+        done();
+      });
+    });
+
+    it('Should return an error on wrong email address and password', (done) => {
+      const user = {
+        email: 'kissass@andela',
+        password: '1234s'
+      };
+      chai.request(server)
+      .post('/api/auth/login')
+      .send(user)
+      .end((err, response) => {
+        response.should.have.status(401);
+        response.body.should.have.property('message');
+        response.body.should.have.property('success');
+        response.body.should.have.property('message').eql('Authentication failed');
+        done();
+      });
+    });
+
+    it('Should return JWT on successful login', (done) => {
+      const user = {
+        email: 'kissa@andela',
+        password: '123456'
+      };
+      chai.request(server)
+      .post('/api/auth/login')
+      .send(user)
+      .end((error, response) => {
+        response.should.have.status(200);
+        response.body.should.have.property('message');
+        response.body.should.have.property('success');
+        response.body.should.have.property('token');
+        done();
+      });
+    });
+  });
 });
+
