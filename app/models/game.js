98270119fd2d833a816e6aa2b27780a5080acdd3
Victor Nwaiwu <victornwaiwu@victor.local>
2016-12-12 14:24:34 +0100

    feature(models): create game model for saving game
    create Game model for saving game history

---
+++ app/models/game.js
@@ -0,0 +1,35 @@
+/**
+ * Module dependencies.
+ */
+const mongoose = require('mongoose'),
+  Schema = mongoose.Schema;
+
+
+/**
+ * Game Schema
+ */
+const GameSchema = new Schema({
+  game_id: {
+    type: String,
+    required: true
+  },
+  creator: {
+    type: String,
+    required: true
+  },
+  winner: {
+    type: String,
+    default: ''
+  },
+  date_created: {
+    type: Date,
+    default: Date.now
+  },
+  completed: {
+    type: String,
+    default: ''
+  },
+  friends: Array,
+});
+
+mongoose.model('Game', GameSchema);
