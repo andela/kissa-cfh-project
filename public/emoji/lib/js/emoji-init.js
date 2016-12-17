3897b987a58b6388f49af45686c47618a5e46a59
andela-tisrael <israel.tomilayo@andela.com>
2016-12-17 11:27:44 +0100

    refactor(clean-up): remove development folders and files from the lib folder finishes #134209541

---
+++ public/emoji/lib/js/emoji-init.js
@@ -0,0 +1,9 @@
+$(function() {
+    // Initializes and creates emoji set from sprite sheet
+    window.emojiPicker = new EmojiPicker({
+        emojiable_selector: '[data-emojiable=true]',
+        assetsPath: '/emoji/lib/img/',
+        popupButtonClasses: 'fa fa-smile-o'
+    });
+    window.emojiPicker.discover();
+});
