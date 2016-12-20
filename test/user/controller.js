f96e529fe20d675d6a7b0cef6b20f4ddf21a416f
andela-nekekwe <ngozi.ekekwe@andela.com>
2016-12-20 18:55:14 +0100

    fix(tests): fix failing tests on travis

--- test/user/controller.js
+++ test/user/controller.js
@@ -6,7 +6,7 @@ const users = require('../../app/controllers/users');
 
 const User = mongoose.model('User');
 
-let user = '';
+var user = '';
 
 describe('User Authentication', () => {
   beforeEach((done) => {
